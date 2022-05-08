56e5520c2e095538c0ce83b6b1e74fd0316277fa
Emilio Vargas Millan <emiliovm@outlook.es>
2022-05-08 10:22:08 +0200

    terminado

---
+++ java.js
@@ -0,0 +1,28 @@
+
+// CÁLCULO DEL IVA
+
+/* let datos = prompt("introduce un numero para calcular el IVA");
+
+let calc = () => datos * 1.21;
+
+document.write (`<h1> el dato introducido, que es ${datos}, sumandole el 21% es ${calc()}</h1>`) */
+
+// DETERMINAR SI UN NÚMERO ES MÚLTIPLO
+
+
+/* let otrosDatos = prompt("¿será multiplo el número introducido?")
+
+function multi() {
+
+    while (true) {
+        if ((otrosDatos % 2) == 0) {
+            alert (`sí que es multiplo de 2`)
+            break
+        } else {
+            otrosDatos = prompt("ERROR, prueba de nuevo")
+        }
+    }
+
+}
+
+multi() */
